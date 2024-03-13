2f40cdf468bc032fa92adf8f44087ca957e55455
BeletskiyPR <BeletskiyPR@408D-9>
2024-03-13 17:10:51 +0700

    Добавьте файлы проекта.

---
+++ LibThread/Robot.cs
@@ -0,0 +1,57 @@
+﻿namespace LibThread
+{
+    public class Robot
+    {
+        public Robot()
+        {
+            new Thread(Move).Start();
+        }
+        public Robot(int i)
+        {
+            Name+=i;
+            Thread.Sleep(50);
+            new Thread(Move).Start();
+        }
+        protected (int x, int y) position = (0, 0);
+        protected string Name { get; set; } = "Робот";
+        public void Move()
+        {
+
+            Random randomMove = new Random();
+            string moveInfo;
+            for (int i = 0; i < 3; i++)
+            {
+                switch (randomMove.Next(3))
+                {
+                    case 0:
+                        position.y++;
+                        moveInfo = "Вперед";
+                        GetInfo(position, moveInfo);
+                        break;
+                    case 1:
+                        position.y--;
+                        moveInfo = "Назад";
+                        GetInfo(position, moveInfo);
+                        break;
+                    case 2:
+                        position.x++;
+                        moveInfo = "Направо";
+                        GetInfo(position, moveInfo);
+                        break;
+                    case 3:
+                        position.x--;
+                        moveInfo = "Налево";
+                        GetInfo(position, moveInfo);
+                        break;
+                    default:
+                        break;
+                }
+                Thread.Sleep(200);
+            }
+        }
+        public void GetInfo((int, int) position, string moveInfo)
+        {
+            Console.WriteLine($"Имя: {Name} Движение: {moveInfo} Позиция: {position} ПотокID: {Thread.CurrentThread.ManagedThreadId}\n");
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,8 @@
+﻿using LibThread;
+//for (int i = 0; i < 3; i++)
+//{
+//    new Robot(i);
+//}
+Robot robot = new Robot();
+Robot robot1 = new Robot();
+Robot robot2 = new Robot();
